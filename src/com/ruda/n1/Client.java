3181c2b7615722716ef6934da86be171b6705c42
Sliver8910 <qodmstn1@naver.com>
2019-09-25 17:33:30 +0900

    09.25 dinner

--- src/com/ruda/n1/Client.java
+++ src/com/ruda/n1/Client.java
@@ -1,9 +1,13 @@
 package com.ruda.n1;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
+//import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Scanner;
 
@@ -12,25 +16,64 @@ public class Client {
 	public static void main(String[] args) {
 		//1단계 
 		Socket sc = null;
+		//Socket sc1 = null;
+		//ServerSocket ss = null;
 		OutputStream os = null;
 		OutputStreamWriter ow = null;
 		BufferedWriter bw = null;
+		InputStream is = null;
+		InputStreamReader ir = null;
+		BufferedReader br = null;
+		String str = null;
 		Scanner ssc = new Scanner(System.in); 
 		try {
 			sc = new Socket("211.238.142.21", 8282);
-			System.out.println("서버로 전송할 메세지 입력");
-			String str = ssc.next();
-			os = sc.getOutputStream();//byte
-			ow = new OutputStreamWriter(os);
-			bw = new BufferedWriter(ow);
-			bw.write(str+"\r\n");
-			bw.flush();
-			System.out.println("서버로 전송 완료");
+			boolean check = true;
+			while(check) {
+				System.out.println("서버로 전송할 메세지 입력");
+				str = ssc.next();
+				os = sc.getOutputStream();//byte
+				ow = new OutputStreamWriter(os);
+				bw = new BufferedWriter(ow);
+				bw.write(str+"\r\n");
+				bw.flush();
+				System.out.println("서버로 전송 완료");
+				if(str.toUpperCase().equals("END")) {
+					break;
+					}
+
+				System.out.println("서버 접속 받을 준비중");
+				is = sc.getInputStream();
+				ir = new InputStreamReader(is);
+				br = new BufferedReader(ir);
+				str = br.readLine();
+				System.out.println(str);
+
+				if(str.toUpperCase().equals("END")) {
+					break;
+				}
+			}
+			/**********
+			ss = new ServerSocket(8281);
+			System.out.println("서버 접속 받을 준비중");
+			sc1 = ss.accept();
+			is = sc1.getInputStream();
+			ir = new InputStreamReader(is);
+			br = new BufferedReader(ir);
+			str = br.readLine();
+			System.out.println(str);
+			 ***********/
+
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} finally {
 			try {
+				br.close();
+				ir.close();
+				is.close();
+				//sc1.close();
+				//ss.close();
 				bw.close();
 				ow.close();
 				os.close();
@@ -42,3 +85,4 @@ public class Client {
 		}
 	}
 }
+
--- src/com/ruda/n1/Server.java
+++ src/com/ruda/n1/Server.java
@@ -1,11 +1,15 @@
 package com.ruda.n1;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Server {
 
@@ -14,23 +18,65 @@ public class Server {
 		//Socket
 		ServerSocket ss = null;
 		Socket sc = null;
+		//Socket sc1 = null;
 		InputStream is = null;
 		InputStreamReader ir = null;
 		BufferedReader br = null;
+		OutputStream os = null;
+		OutputStreamWriter ow = null;
+		BufferedWriter bw = null;
+		Scanner ssc = new Scanner(System.in);
+		String str = null;
 		try {
 			ss = new ServerSocket(8282);
-			System.out.println("클라이언트 접속 받을 준비중");
-			sc = ss.accept();
-			is = sc.getInputStream();//byte
-			ir = new InputStreamReader(is);//char
-			br = new BufferedReader(ir); //char를 모아서 String으로 만들어줌
-			String str = br.readLine();
-			System.out.println(str);
+			boolean check = true;
+			while(check) {
+				System.out.println("클라이언트 접속 받을 준비중");
+				sc = ss.accept();
+				is = sc.getInputStream();//byte
+				ir = new InputStreamReader(is);//char
+				br = new BufferedReader(ir); //char를 모아서 String으로 만들어줌
+				str = br.readLine();
+				System.out.println(str);
+				if(str.toUpperCase().equals("END")) {
+					break;
+					}
+
+				System.out.println("클라이언트로 전송할 메세지 입력");
+				str = ssc.next();
+				os = sc.getOutputStream();
+				ow = new OutputStreamWriter(os);
+				bw = new BufferedWriter(ow);
+				bw.write(str+"\r\n");
+				bw.flush();
+				System.out.println("클라이언트로 전송 완료");
+				if(str.toUpperCase().equals("END")) {
+					break;
+				}
+			}
+
+
+			/**********
+			sc1 = new Socket("211.238.142.21", 8281);
+			System.out.println("서버로 전송할 메세지 입력");
+			str = ssc.next();
+			os = sc1.getOutputStream();
+			ow = new OutputStreamWriter(os);
+			bw = new BufferedWriter(ow);
+			bw.write(str+"\r\n");
+			bw.flush();
+			System.out.println("클라이언트로 전송 완료");
+			 *************/
+
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}finally {
 			try {
+				bw.close();
+				ow.close();
+				os.close();
+				//sc1.close();
 				br.close();
 				ir.close();
 				is.close();
